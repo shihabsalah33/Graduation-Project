d62845052b8285990e01702f9253766dc9885350
Shihab <shihab@example.com>
2026-08-04 17:58:07 +0300

    Auto Sync: Local update [2026-08-04 17:58]

--- install_service.bat
+++ install_service.bat
@@ -4,13 +4,12 @@ set "SCRIPT_PATH=%~dp0scripts\auto_sync_service.py"
 
 echo Setting up Auto Sync Task in Windows Scheduled Tasks...
 
-schtasks /create /tn "%TASK_NAME%" /tr "pythonw.exe \"%SCRIPT_PATH%\"" /sc onstart /ru "%USERNAME%" /f > nul 2>&1
+schtasks /create /tn "%TASK_NAME%" /tr "pythonw.exe \"%SCRIPT_PATH%\"" /sc minute /mo 1 /f > nul 2>&1
 if %errorlevel% neq 0 (
-    schtasks /create /tn "%TASK_NAME%" /tr "python.exe \"%SCRIPT_PATH%\"" /sc onstart /ru "%USERNAME%" /f > nul 2>&1
+    schtasks /create /tn "%TASK_NAME%" /tr "python.exe \"%SCRIPT_PATH%\"" /sc minute /mo 1 /f > nul 2>&1
 )
 
 echo Running service now...
 schtasks /run /tn "%TASK_NAME%" > nul 2>&1
 
 echo Auto Sync Service installed successfully!
-
